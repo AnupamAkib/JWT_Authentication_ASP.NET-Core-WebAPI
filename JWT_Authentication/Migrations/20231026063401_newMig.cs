3e28505ea5a6e3f7e98cff213c97f1e2a5fd4ac2
AnupamAkib <mirakib25@gmail.com>
2023-10-26 12:34:37 +0600

    newly migrated

---
+++ JWT_Authentication/Migrations/20231026063401_newMig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWT_Authentication.Migrations
+{
+    /// <inheritdoc />
+    public partial class newMig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
